55cee29041caa39a0c483d2e97ee6d591b41d4a7
mikhael.santana <mikhael.santana@sergipetec.org.br>
2025-11-15 15:23:18 -0300

    feat: adicionei novas imagens e atualizei informações de localização do estúdio

--- src/components/mapsection/MapSection.tsx
+++ src/components/mapsection/MapSection.tsx
@@ -40,7 +40,7 @@ const MapSection: React.FC = () => {
 
           <div className="map-section__map">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2985.886392920909!2d-8.422062!3d41.550056999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24ff5d1450def7%3A0x278d08c027a9465a!2sBrian%20Dantas%20Tattoo!5e0!3m2!1spt-BR!2sbr!4v1752101694260!5m2!1spt-BR!2sbr"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3000.9698795913666!2d-8.429054!3d41.549986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd24fea882e0d115%3A0x5c6b4c0b8c2b4c0b!2sRua%20do%20Carmo%2079%2C%204700-309%20Braga!5e0!3m2!1spt-BR!2sbr!4v1731687600000!5m2!1spt-BR!2sbr"
               className="google-map-iframe"
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
